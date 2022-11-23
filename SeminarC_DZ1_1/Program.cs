b7dcfa3bb6fe2112bb1d4b816bf678a02d2d78f4
unknown <pogodinaya@mail.ru>
2022-11-23 19:03:38 +0300

    init: Задача на вывод максимального из 2 чисел

---
+++ SeminarC_DZ1_1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Clear();
+
+Console.WriteLine("Введите число 1:");
+
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2:");
+
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA < numB)
+{
+    Console.WriteLine($"max = {numB} min = {numA}");
+}
+else if (numA > numB)
+{
+    Console.WriteLine($"max = {numA} min = {numB}");
+}
+else if (numA == numB)
+{
+    Console.WriteLine("Значения равны");
+}
