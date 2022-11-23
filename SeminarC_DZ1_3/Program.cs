2a7107355af5037f0e1aec493091ffed4d55c9ea
unknown <pogodinaya@mail.ru>
2022-11-23 20:59:26 +0300

    Задача 3

--- SeminarC_DZ1_3/Program.cs
+++ SeminarC_DZ1_3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+
+Console.Clear();
+
+int vvod;
+
+Console.WriteLine("Введите целое число");
+
+vvod = Convert.ToInt32(Console.ReadLine());
+
+if (vvod < 0)
+{
+    vvod = vvod * (-1);
+}
+
+if ((vvod % 2) == 0)
+{
+    Console.WriteLine("Это четное число");
+}
+
+else if ((vvod % 2) > 0)
+{
+    Console.WriteLine("Это не четное число");
+}
