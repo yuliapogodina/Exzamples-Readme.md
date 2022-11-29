2f3b68d867d50c8a223956e2ef74bce93ca4c036
unknown <pogodinaya@mail.ru>
2022-11-29 22:06:31 +0300

    ДЗ семинар задача 23

---
+++ SeminarC_DZ3_3/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход 2 числа (N,M) и выдаёт таблицу кубов чисел от N до M.
+//2,3 -> 8, 27
+//1,5 -> 1, 8, 27, 64, 125
+
+double Exponent(int number, int x)
+{
+    double a = Math.Pow(number, x);
+    return a;
+}
+
+
+Console.Write("Введите число 1:");
+int startnumber = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Введите число 2, больше предыдущего:");
+int finishnumber = Convert.ToInt32(Console.ReadLine());
+
+int exp = 3; // можно запросить ввод этого параметра
+int i;
+for (i = startnumber; i <= finishnumber; i++)
+{
+    double result = Exponent(i, exp);
+   Console.WriteLine(result);
+}
