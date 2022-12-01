4b4b80ed90510404a91af10126ef0e521e45edf1
unknown <pogodinaya@mail.ru>
2022-12-01 22:20:27 +0300

    DZC# 3 2

--- SeminarC_DZ3_2/Program.cs
+++ SeminarC_DZ3_2/Program.cs
@@ -1,37 +1,39 @@
-﻿//int num = 123421;
-//int i = 6;
+﻿//Является ли число полиндромом: 12321 - да,  123421 -  нет
 
 Console.Write("Введите число: ");
 string numstring = (Console.ReadLine() ?? " ");
-int i = 0;
-int num;
-int num1=0;
-int j;
+Console.WriteLine(numstring == "" ? "Ничего не было введено!" : "");
 
-if (numstring == "")
+// определение разрядности числа
+int FindCounNumber(string x)
 {
-    Console.Write("Ничего не было введено!");
-}
-else
-{
-    num = Convert.ToInt32(numstring);
-    while (num > 0)
+    int input = Convert.ToInt32(x);
+    int r = 0;
+    while (input > 0)
     {
-        i++;  // и есть разрядность
-        num /= 10;
+        r++;  // и есть разрядность
+        input /= 10;
     }
+    return r;
+}
+// развернуть число в обратном порядк
+int GetTurnNumber(int inputnumber, int count)
+{
+    int i = count;
+    int outputnum = 0;
+    for (int j = 0; j < count; j++)
+    {
+        --i;
+        int b = Convert.ToInt32(Math.Pow(10, i));
+        int a = inputnumber / b;
+        outputnum = outputnum + a * Convert.ToInt32(Math.Pow(10, j));
+        inputnumber = inputnumber - a * b;
+    }
+    return outputnum;
 }
 
-num = Convert.ToInt32(numstring);
-int count = i;
-// пересобрать число в обратном порядке
-for (j=0; j<count; j++)
-{
---i;
-int b = Convert.ToInt32(Math.Pow(10, i));
-int a = num / b;
-num1 = num1 + a*Convert.ToInt32(Math.Pow(10, j));
-num = num - a*b;
-}
-num = Convert.ToInt32(numstring);
-Console.WriteLine(num1==num ? "Полиндром": "Не полиндром");
+
+// преобразование строки в число
+int num = Convert.ToInt32(numstring);
+
+Console.WriteLine(GetTurnNumber(num, FindCounNumber(numstring)) == num ? "Полиндром" : "Не полиндром");
