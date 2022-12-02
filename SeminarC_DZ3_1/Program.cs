78e1c9fddc130b4a83a94377751a5a2eef25ef8b
unknown <pogodinaya@mail.ru>
2022-12-02 21:50:20 +0300

    DZ 3_1

---
+++ SeminarC_DZ3_1/Program.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+int[] IntCoord(int n)
+{
+    int[] coord = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Ведите координату: ");
+        coord[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return coord;
+}
+
+Console.WriteLine("Точка 1");
+int[] point1 = IntCoord(3);
+Console.WriteLine("Точка 1");
+int[] point2 = IntCoord(3);
+
+double SumSqrValue(int[] a, int[] b)
+{
+    double sum = 0;
+    for (int j = 0; j < point1.Length; j++)
+    {
+        sum = Math.Pow((point1[j] - point2[j]), 2) + sum;
+    }
+    sum = Math.Sqrt(sum);
+    return sum;
+}
+double result = SumSqrValue(point1, point2);
+Console.Write($"Расстрояние между точками = {string.Format("{0:f1}", result)}");
