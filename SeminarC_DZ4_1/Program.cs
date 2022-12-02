72a3f56653d4820b49ad265992506d0e5f519c58
unknown <pogodinaya@mail.ru>
2022-12-02 22:26:26 +0300

    02/12/22

---
+++ SeminarC_DZ4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Clear();
+Console.Write("Ведите число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите степень числа: ");
+int exp = Convert.ToInt32(Console.ReadLine());
+
+double Exponent(double x, int y)
+{
+    double expofx = 1;
+    for (int i = 1; i <= y; i++) expofx = expofx * x;
+    return expofx;
+}
+
+Console.WriteLine($"Число {number} в степени {exp} = {Exponent(number, exp)}");
