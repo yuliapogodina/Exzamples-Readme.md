4a2dcd32147d06bdbc1289c4e8abcee4e13de518
unknown <pogodinaya@mail.ru>
2022-12-04 15:29:33 +0300

    DZC# 4 2

--- SeminarC_DZ4_2/Program.cs
+++ SeminarC_DZ4_2/Program.cs
@@ -7,36 +7,18 @@ Console.Clear();
 
 Console.Write("Введите число отличное от нуля: ");
 string numstring = (Console.ReadLine() ?? " ");
-int i = 0;
-Console.WriteLine(numstring == "" ? "Ничего не было введено!" : " ");//i = FindCounNumber(numstring));
+Console.WriteLine(numstring == "" ? "Ничего не было введено!" : " ");
 
-int FindCounNumber(string x)
-{
-
-    int num = Convert.ToInt32(x);
-    while (num > 0)
-    {
-        i++;  // и есть разрядность
-        num /= 10;
-    }
-
-    return i;
-}
-
-int SumOfDigit(int n, string a)
+int GetSumOfDigit(string a)
 {
     int num = Convert.ToInt32(a);
     int sumdigit = 0;
-    for(int i = n-1; i < 0; --i)
-    {        
-        int digit = num / Convert.ToInt32(Math.Pow(10, i));
-        num = num - digit * Convert.ToInt32(Math.Pow(10, i));
-        sumdigit = sumdigit + digit;
-        Console.WriteLine(num);
+    while (num > 0)
+    {
+        sumdigit = sumdigit + num % 10;
+        num = num / 10;
     }
     return sumdigit;
 }
 
-int result = SumOfDigit(i, numstring);
-//Console.WriteLine(String.Join(",", arr));
-Console.WriteLine(result);
+Console.WriteLine($"Сумма цифр в числе {numstring} = {GetSumOfDigit(numstring)}");
