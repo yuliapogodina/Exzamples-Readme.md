52ab80213113799d8b55eebfa71182a668665292
unknown <pogodinaya@mail.ru>
2022-12-04 16:01:28 +0300

    DZC# 4 3

---
+++ SeminarC_DZ4_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите размерность масива:");
+int arrlenhgt = Convert.ToInt32(Console.ReadLine());
+
+int[] FillingArray(int n)
+{
+    Random rnd = new Random();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rnd.Next(0, 10);
+    }
+    return arr;
+}
+
+int[] arr = FillingArray(arrlenhgt);
+Console.WriteLine(String.Join(",", arr));
