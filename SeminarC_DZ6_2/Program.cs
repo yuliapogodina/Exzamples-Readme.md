6225361deb05e0dd364a9af99bce5b5360196b12
unknown <pogodinaya@mail.ru>
2022-12-13 21:29:19 +0300

    изменение типа данных

--- SeminarC_DZ6_2/Program.cs
+++ SeminarC_DZ6_2/Program.cs
@@ -4,19 +4,18 @@
 Console.Clear();
 Console.WriteLine("Поиск точки пересечения двух уравнений прямой y=kx+b");
 Console.WriteLine("Введите параметры уровнения 1: Коэффициент (k1) и добавочное число (b1)");
-int k1 = Convert.ToInt32(Console.ReadLine());
-int b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToDouble(Console.ReadLine());
+double b1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите параметры уровнения 2: Коэффициент (k2) и добавочное число (b2)");
-int k2 = Convert.ToInt32(Console.ReadLine());
-int b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
+double b2 = Convert.ToDouble(Console.ReadLine());
 
-(int, int) FindIntersectionCoodinate(int coefficient1, int coefficient2, int correct1, int correct2)
+(double, double) FindIntersectionCoodinate(double coefficient1, double coefficient2, double correct1, double correct2)
 {
-    int x;
-    if ((correct1 - correct2) == 0) x = -1 * (coefficient1 - coefficient2);
-    else x = -1 * (coefficient1 - coefficient2) / (correct1 - correct2);
-    int y = coefficient1 * x + correct1;
+    double x;
+    x = (correct2 - correct1)/(coefficient1 - coefficient2);
+    double y = coefficient1 * x + correct1;
     return (x, y);
 }
 
-Console.WriteLine($"Координата x: {FindIntersectionCoodinate(k1, k2, b1, b2).Item1}, Координата y: {FindIntersectionCoodinate(k1, k2, b1, b2).Item2}");
+Console.WriteLine($"Координата ({FindIntersectionCoodinate(k1, k2, b1, b2).Item1}; {FindIntersectionCoodinate(k1, k2, b1, b2).Item2})");
