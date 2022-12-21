db6cff238b4ea782c7502e25064f8f91e6dec963
unknown <pogodinaya@mail.ru>
2022-12-21 19:28:21 +0300

    НОД рекурсия

--- SeminarC_DZ9_3/Program.cs
+++ SeminarC_DZ9_3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+// M = 28; N = 7 -> 7
+
+Console.Clear();
+Console.Write("Введите число M : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Наибольший общий делитель чисел M = {m}, N = {n} -> {CalculateDivisorOfDigits(m, n)}");
+
+int CalculateDivisorOfDigits(int a, int b)
+{
+    if (a == 0) return b;
+    else{
+    if (a > b) (a, b) = (b, a);
+    return CalculateDivisorOfDigits(b - a, a);}
+}
